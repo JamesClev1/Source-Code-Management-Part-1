15064fb8aef2774eef2efebe4646a99f17ff0fe4
James C <james.james@gmail.com>
2023-03-14 15:11:30 -0400

    First Commit

---
+++ Source Code Management  Part 1/Source.cpp
@@ -0,0 +1,16 @@
+// James Clevenger 
+// Source Code Management 
+// CIS 1202
+// 03/14/2023
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	cout << "Hello World" << endl;
+	return 0;
+
+}
